8d8c10f00495b3b2715bd083a3dd982acdc9fcd7
Matt Cole <matt.s.cole@outlook.com>
2017-10-10 13:59:53 -0400

    added jenkins server build

--- instance.tf
+++ instance.tf
@@ -1,4 +1,14 @@
-resource "aws_instance" "example" {
+resource "aws_key_pair" "jenkinskey" {
+  key_name = "jenkinskey" 
+  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
+}
+
+resource "aws_instance" "Jenkins" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "${lookup(var.AWS_EC2_INSTANCE)}"
+  key_name = "${aws_key_pair.jenkinskey.key_name}"
+}
+
+provisioner "local-exec" { 
+  command = "sleep 30 && echo -e \"[Jenkins]\n${aws_instance.Jenkins.public_ip} ansible_connection=ssh ansible_ssh_user=root\" > inventory &&  ansible-playbook -i inventory ./ansible/playbook.yml"
 }
