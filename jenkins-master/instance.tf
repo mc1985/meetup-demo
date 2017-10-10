dde4abf680215d5517dcc5cb0f4a208a0f96466a
Matt Cole <matt.s.cole@outlook.com>
2017-10-10 14:21:48 -0400

    moved jenkins to its own directory

--- jenkins-master/instance.tf
+++ jenkins-master/instance.tf
@@ -3,12 +3,12 @@ resource "aws_key_pair" "jenkinskey" {
   public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
 }
 
-resource "aws_instance" "app-server" {
+resource "aws_instance" "Jenkins" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "${lookup(var.AWS_EC2_INSTANCE)}"
   key_name = "${aws_key_pair.jenkinskey.key_name}"
 }
 
 provisioner "local-exec" { 
-  command = "sleep 30 && echo -e \"[app-server]\n${aws_instance.app-server.public_ip} ansible_connection=ssh ansible_ssh_user=root\" > inventory &&  ansible-playbook -i inventory ./ansible/playbook.yml"
+  command = "sleep 30 && echo -e \"[Jenkins]\n${aws_instance.Jenkins.public_ip} ansible_connection=ssh ansible_ssh_user=root\" > inventory &&  ansible-playbook -i inventory ./ansible/playbook.yml"
 }
---
+++ jenkins-master/terraform.tfvars
@@ -0,0 +1,3 @@
+AWS_ACCESS_KEY="your access key"
+AWS_SECRET_KEY="your secret key"
+AWS_EC2_INSTANCE="your instance size"
