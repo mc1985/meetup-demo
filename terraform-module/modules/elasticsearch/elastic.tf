18df2dbc3c0f0f9a1db3da85d0dfd38b52788827
Matt Cole <matt.s.cole@outlook.com>
2017-11-23 08:48:08 -0500

    updated module

--- terraform-module/modules/elasticsearch/elastic.tf
+++ terraform-module/modules/elasticsearch/elastic.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "server" {
     ami = "${lookup(var.ami, "${var.region}-${var.platform}")}"
     instance_type = "${var.instance_type}"
-    key_name = "${var.key_name}"
     count = "${var.servers}"
 
     #Instance tags
@@ -9,19 +8,4 @@ resource "aws_instance" "server" {
         Name = "${var.tagName}-${count.index}"
     }
 
-    // These are for maintenance
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    // This is for outbound internet access
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
 }
--- terraform-module/modules/elasticsearch/vars.tf
+++ terraform-module/modules/elasticsearch/vars.tf
@@ -19,15 +19,6 @@ variable "ami" {
   }
 }
 
-
-variable "key_name" {
-  description = "SSH key name in your AWS account for AWS instances."
-}
-
-variable "key_path" {
-  description = "Path to the private key specified by key_name."
-}
-
 variable "region" {
   default     = "us-east-1"
   description = "The region of AWS, for AMI lookups."
